30852b0f24a13bf0e3bce5574a7e7780c88a3c39
Sebastian Pino <seba.kodk@gmail.com>
2022-10-14 17:28:34 -0300

    Calendario
    Agregué un calendario que marca la fecha actual (Seteada manualmente). Tiene vistas por mes, semanas, 4 dias y solo un dia. Aun faltan los eventos

---
+++ Frontend/src/components/PrincipalUsuario/Calendario.vue
@@ -0,0 +1,118 @@
+<template>
+    <v-row>
+      <v-col>
+        <!-- Barra superior del calendario -->
+        <v-sheet height="64">
+        <v-toolbar
+          flat
+        >
+          <v-btn
+            outlined
+            class="mr-4"
+            color="grey darken-2"
+            @click="setToday"
+          >
+            Today
+          </v-btn>
+          <v-btn
+            fab
+            text
+            small
+            color="grey darken-2"
+            @click="prev"
+          >
+            <v-icon small>
+              mdi-chevron-left
+            </v-icon>
+          </v-btn>
+          <v-btn
+            fab
+            text
+            small
+            color="grey darken-2"
+            @click="next"
+          >
+            <v-icon small>
+              mdi-chevron-right
+            </v-icon>
+          </v-btn>
+          <v-toolbar-title v-if="$refs.calendar">
+            {{ $refs.calendar.title }}
+          </v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-menu
+            bottom
+            right
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                outlined
+                color="grey darken-2"
+                v-bind="attrs"
+                v-on="on"
+              >
+                <span>{{ typeToLabel[type] }}</span>
+                <v-icon right>
+                  mdi-menu-down
+                </v-icon>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item @click="type = 'day'">
+                <v-list-item-title>Day</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click="type = 'week'">
+                <v-list-item-title>Week</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click="type = 'month'">
+                <v-list-item-title>Month</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click="type = '4day'">
+                <v-list-item-title>4 days</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-toolbar>
+      </v-sheet>
+
+      <!-- calendario -->
+        <v-sheet height="400">
+          <v-calendar
+            ref="calendar"
+            :now="today"
+            :value="today"
+            :events="events"
+            color="primary"
+            :type="type"
+          ></v-calendar>
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </template>
+
+<script>
+export default {
+  data: () => ({
+    typeToLabel: {
+    month: 'Month',
+    week: 'Week',
+    day: 'Day',
+    '4day': '4 Days',
+    },
+    type: 'month',
+    selectedOpen: false,
+    today: '2019-01-08',    //Hacer dinamico
+    events: [],
+
+  }),
+  mounted () {
+    this.$refs.calendar.checkChange()
+  },
+  methods:{
+    setToday () {
+        this.focus = ''
+      },
+  }
+}
+</script>
+
--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -4,6 +4,7 @@ import Home from '../components/Home.vue'
 import Nosotros from '../components/Nosotros.vue'
 import InicioSesion from '../components/InicioSesion.vue'
 import Principal from '../components/Principal.vue'
+import Calendario from '../components/PrincipalUsuario/Calendario.vue'
 Vue.use(VueRouter)
 const routes = [
   {
@@ -35,7 +36,11 @@ const routes = [
     name: 'usuario',
     component: () => import('../components/PrincipalUsuario/VistaUsuario.vue')
   },
- 
+  {
+    path: '/calendario',
+    name: 'Calendario',
+    component: () => import('../components/PrincipalUsuario/Calendario.vue')
+  },
 ]
 const router = new VueRouter({
   routes

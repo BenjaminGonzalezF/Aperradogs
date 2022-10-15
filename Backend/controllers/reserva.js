9187b08b8dd9a1cd37133db2446c113b74cf7b48
BenjaminGonzalezF <begonzalez18@alumnos.utalca.cl>
2022-10-14 22:24:04 -0300

    Avance creación de reserva

---
+++ Backend/controllers/reserva.js
@@ -0,0 +1,36 @@
+import { reserva } from "../data_base/models/reserva.js";
+
+
+
+
+let id=0
+const guardarReserva = async (datosNuevaReserva) => {
+    let id = await obtenerNumeroReservas();
+    const nuevaReserva = new reserva({
+        ID: id,
+        Hora: datosNuevaReserva.hora,
+        Fecha: datosNuevaReserva.fecha,
+        Cliente: datosNuevaReserva.cliente,
+        Mascota: datosNuevaReserva.mascota,
+    })
+    nuevaMascota.save();
+    console.log("Reserva guardada: ", nuevaMascota);
+};
+
+//obtener numero de reservas en la base de datos
+const obtenerNumeroReservas = async () => {
+    let query = await reserva.find();
+    let numeroReserva = query.length;
+    return numeroReserva;
+}
+
+
+const mostrarReservas = () => {
+    mascota.find({}, (err, reservas) => {
+        if (err) {
+            console.log('Error al mostrar las reservas');
+        } else {
+            console.log(reservas);
+        }
+    });
+};

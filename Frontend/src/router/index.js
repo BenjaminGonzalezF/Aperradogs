a692826ff4492728c2299c9026246212f8d674e6
BenjaminGonzalezF <begonzalez18@alumnos.utalca.cl>
2022-10-18 23:23:49 -0300

    Incorporación de avances

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -52,7 +52,7 @@ const routes = [
   {
     path: '/fichaClinica',
     name: 'admin',
-    component: FichaClinica
+    component: () => import('../components/FichaClinica.vue')
   },
 ]
 const router = new VueRouter({
